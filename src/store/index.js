a5214e401af024a354d346fbb1b5137a3d7aec3f
litingchen <2496272528@qq.com>
2020-07-08 17:57:57 +0800

    添加sessionStorage存state里面的变量

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isCollapae: false,
+    isCollapae: JSON.parse(sessionStorage.getItem('isCollapae')) || false,
   },
   mutations: {
     SET_COLLAPSE (state) {
       state.isCollapae = !state.isCollapae
+      window.sessionStorage.setItem('isCollapae', JSON.stringify(state.isCollapae))
     }
   },
   getters: {
