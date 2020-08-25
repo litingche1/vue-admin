1b5c44a120e371b8a2b5bc759632048e90af5088
李廷琛 <2496272528@qq.com>
2020-08-26 00:54:24 +0800

    完成信息分类页面的编写

--- src/views/Infor/inforCategory.vue
+++ src/views/Infor/inforCategory.vue
@@ -5,40 +5,78 @@
     <div>
       <el-row :gutter="30">
         <el-col :span="8">
-          <div class="category">
-            <h4>
-            <!-- <svg-icon iconClass="minus"></svg-icon> -->
-                <svg-icon iconClass="minus" iconName="minus" />
-              新闻</h4>
-            <ul>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-            </ul>
-          </div>
-          <div class="category">
-            <h4>新闻</h4>
-            <ul>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-              <li>国内</li>
-            </ul>
+          <div class="category-wrap">
+            <div class="category">
+              <h4>
+                <svg-icon iconClass="plus" iconName="plus" />新闻
+                <div class="button-group">
+                  <el-button size="mini" type="danger" round>编辑</el-button>
+                  <el-button size="mini" type="success" round>添加子级</el-button>
+                  <el-button size="mini" round>编辑</el-button>
+                </div>
+              </h4>
+              <ul>
+                <li>
+                  国内
+                  <div class="button-group">
+                    <el-button size="mini" type="danger" round>编辑</el-button>
+                    <el-button size="mini" round>编辑</el-button>
+                  </div>
+                </li>
+                <li>国内</li>
+                <li>国内</li>
+                <li>国内</li>
+                <li>国内</li>
+              </ul>
+            </div>
+            <div class="category">
+              <h4>
+                <svg-icon iconClass="plus" iconName="plus" />新闻
+              </h4>
+              <ul>
+                <li>国内</li>
+                <li>国内</li>
+                <li>国内</li>
+                <li>国内</li>
+                <li>国内</li>
+              </ul>
+            </div>
           </div>
         </el-col>
-        <el-col :span="16"> </el-col>
+        <el-col :span="16">
+          <h4 class="menu-title">一级编辑分类</h4>
+          <el-form  label-width="142px" class="from-warp">
+            <el-form-item label="一级分类名称">
+              <el-input v-model="formLabelAlign.name"></el-input>
+            </el-form-item>
+            <el-form-item label="二级分类名称">
+              <el-input v-model="formLabelAlign.region"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="danger">确定</el-button>
+            </el-form-item>
+          </el-form>
+        </el-col>
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import { reactive } from "@vue/composition-api";
 export default {
-  name: 'inforCategory'
-}
+  name: "inforCategory",
+  setup() {
+    const formLabelAlign = reactive({
+      name: "",
+      region: "",
+      type: ""
+    });
+    return {
+      formLabelAlign
+    };
+  }
+};
 </script>
 <style lang="scss" scoped>
 #inforCategory-warp {
@@ -50,11 +88,34 @@ export default {
     margin-top: 30px;
     margin-bottom: 30px;
   }
+  .category-wrap {
+    div:first-child {
+      &::before {
+        top: 20px;
+      }
+    }
+    div:last-child {
+      &::before {
+        bottom: 21px;
+      }
+    }
+  }
+  .menu-title {
+    background: #f3f3f3;
+    line-height: 44px;
+    padding-left: 22px;
+    margin-bottom: 26px;
+  }
+   .from-warp{
+      width:420px;
+      
+    }
   .category {
     line-height: 44px;
     position: relative;
+    cursor: pointer;
     &:before {
-      content: '';
+      content: "";
       position: absolute;
       width: 32px;
       left: 22px;
@@ -66,24 +127,43 @@ export default {
       padding-left: 40px;
       position: relative;
     }
-    svg{
+    svg {
       position: absolute;
-      // font-size:16px;
-      top:15px;
-      left:15px;
-
+      font-size: 17px;
+      top: 14px;
+      left: 15px;
     }
     li {
       padding-left: 36px;
       margin-left: 24px;
       position: relative;
       &:before {
-        content: '';
+        content: "";
         position: absolute;
         width: 32px;
         left: 0;
         top: 22px;
         border-bottom: 1px dotted #000;
+      }
+    }
+    li,
+    h4 {
+      @include webkit(transition, all 0.3s ease 0s);
+      &:hover {
+        background: #f3f3f3;
+        .button-group {
+          display: block;
+        }
+      }
+    }
+    .button-group {
+      display: none;
+      position: absolute;
+      z-index: 2;
+      right: 11px;
+      top: -1px;
+      button {
+        font-size: 12px;
       }
     }
   }
